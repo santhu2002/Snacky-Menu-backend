7c1513ac345c76dc42add9447e5c2ef26bdbfba6
Santhu <purisanthosh2002@gmail.com>
2023-11-08 15:19:50 +0530

    database url updated

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = "mongodb://localhost:27017/Restaraunt"
+// const mongoURI = "mongodb://localhost:27017/Restaraunt"
+const mongoURI = "mongodb+srv://Santhosh:root@cluster0.xvefb4s.mongodb.net/"
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI, ()=>{
